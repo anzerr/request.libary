6ac87e7694e9bb81eb7b599ddc7e751ba677c0ac
anzerr <ryan.anzer@epitech.eu>
2019-01-22 15:37:31 +0100

    dump

--- src/request.js
+++ src/request.js
@@ -17,8 +17,8 @@ class Request {
 		this.url = url.parse(base);
 		this._headers = {};
 		this._options = {};
-		this.query = null;
 		this.data = {
+			query: null,
 			value: null,
 			type: null
 		};
@@ -36,8 +36,8 @@ class Request {
 			for (let i in this._options) {
 				option[i] = this._options[i];
 			}
-			if (this.query) {
-				option.path += '?' + this.query;
+			if (this.data.query) {
+				option.path += '?' + this.data.query;
 			}
 
 			// console.log(this.url, option, isHttps, this.data);
@@ -63,7 +63,7 @@ class Request {
 	}
 
 	query(data) {
-		this.query = querystring.stringify(data);
+		this.data.query = querystring.stringify(data);
 		return this;
 	}
 
