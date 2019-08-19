e6536c5d45389904c436d6a552fbe5f3308d394b
anzerr <ryan.anzer@epitech.eu>
2019-08-19 16:17:23 +0200

    🚧 added type

---
+++ index.d.ts
@@ -0,0 +1,30 @@
+
+declare class Response {
+
+	headers(key?: string): {[key: string]: string};
+	status(): number;
+	isStatus(n: number): boolean;
+	isOkay(): boolean;
+	body(): any;
+	parse(): any;
+
+}
+
+declare class Request {
+
+	constructor(host: string);
+	query(data: {[key: string]: string | number | boolean}): Request;
+	json(data: any): Request;
+	form(data: any): Request;
+	data(data: string | Buffer): Request;
+	options(data: {[key: string]: string | number | boolean}): Request;
+	headers(data: {[key: string]: string | number}): Request;
+	get(url: string): Promise<Response>;
+	post(url: string): Promise<Response>;
+	put(url: string): Promise<Response>;
+	delete(url: string): Promise<Response>;
+	download(url: string, path: string): Promise<void>;
+
+}
+
+export default Request
